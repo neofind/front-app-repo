21be41904b561b836c73e3751b1ca1078792f1c2
EC2 Default User <ec2-user@ip-172-31-46-195.ap-northeast-2.compute.internal>
2022-01-22 15:35:54 +0000

    ecr test

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog--OktankABP--ecrtest!!!!!!!!!!!!!
+            EKS DEMO Blog--OktankABP--ecrtest!!!!!!
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
